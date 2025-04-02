f811b725c7df628e582ca7d401b13027ea3e7041
Sujal <web.sujal@gmail.com>
2025-04-02 18:44:43 +0000

    Add environment configuration, database connection, and authentication routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on port ", PORT);
+  console.log("Server is running on port:", PORT);
+  connectDB();
 });
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = Router();
+
+router.get("/signup", signup);
+
+router.get("/login", login);
+
+router.get("/logout", logout);
 
 export default router;
