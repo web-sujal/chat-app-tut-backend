52b974f625b53682b1a8fc0aa7474a310bad1e23
Sujal <web.sujal@gmail.com>
2025-04-02 18:15:45 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+const PORT = 5001;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
+});
