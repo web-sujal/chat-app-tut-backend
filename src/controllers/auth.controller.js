4e6e92176acbe7198e5dfe1278f8897040818098
Sujal <web.sujal@gmail.com>
2025-04-05 14:55:08 +0000

    Implement checkAuth functionality to verify user authentication status

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -116,3 +116,12 @@ export const updateProfile = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const checkAuth = (req, res) => {
+  try {
+    return res.status(200).json(req.user);
+  } catch (error) {
+    console.error("Error in checkAuth controller: ", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,7 @@ import {
   logout,
   signup,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
@@ -16,5 +17,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check-auth", protectRoute, checkAuth);
 
 export default router;
